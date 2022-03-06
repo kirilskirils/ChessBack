be8269c98bc29f6cbd21cba66dd497b8b92f3ab5
myklek <69005207+myklek@users.noreply.github.com>
2022-03-07 00:56:10 +0200

    Project rebuild

---
+++ src/main/java/com/chess/chessbackend/ChessBackendApplication.java
@@ -0,0 +1,15 @@
+package com.chess.chessbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChessBackendApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(ChessBackendApplication.class, args);
+    }
+
+}
